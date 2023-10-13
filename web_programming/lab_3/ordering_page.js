32bd2f4a34de3f13866fb16663dda40c04e96f73
love_with <rbordulyak@gmail.com>
2023-10-13 13:27:36 +0300

    adding third lab

---
+++ web_programming/lab_3/ordering_page.js
@@ -0,0 +1,4 @@
+document.getElementById('basket').addEventListener('click', function () {
+    // Використовуємо метод location.href для зміни URL і переходу на нову сторінку
+    window.location.href = 'index_lab_3.html';
+});
